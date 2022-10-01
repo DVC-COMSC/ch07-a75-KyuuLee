b128b4919012bb20de094e2348b98966ebcb49ba
Kyuu <69904775+KyuuLee@users.noreply.github.com>
2022-10-01 13:19:18 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import sys
 
 num1 = list(map(int, input().split()))
@@ -7,8 +6,14 @@ num2 = list(map(int, input().split()))
 if len(num1) > len(num2):
 	print ('False')
 	sys.exit(0)
-# ******************************
-# Make your Code
-# ******************************
 
-# print ('True') or ('False')
+idx = 0
+for i in range(len(num2)-len(num1)):
+	for j in range(len(num1)):
+		if num2[i+j] != num1[j]:
+			break
+	else:
+		print ('True')
+		break
+else:
+	print ('False')
